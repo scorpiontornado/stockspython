79079b003df3be6bd122091ab8d55886ec03ab10
scorpiontornado <nicholas@Nicholass-Macbook-Air-572.local>
2020-03-21 17:22:12 +1100

    added reference link to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 # 【sss】
 # https://packaging.python.org/discussions/install-requires-vs-requirements/
-install_requires=[
-   'selenium',
-   'lxml'
+# https://stackoverflow.com/questions/46877667/how-to-push-a-new-initial-project-to-github-using-vs-code
+install_requires = [
+    'selenium',
+    'lxml'
 ]
