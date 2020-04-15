42b890b7503f0ccd09225f8073e6ce8038b95cf0
scorpiontornado <nicholas@Nicholass-Macbook-Air-572.local>
2020-04-15 18:45:36 +1000

    setup.py changes (I'm still trying to figure this out...)

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 # 【sss】
 # https://packaging.python.org/discussions/install-requires-vs-requirements/
 # https://stackoverflow.com/questions/46877667/how-to-push-a-new-initial-project-to-github-using-vs-code
-install_requires = [
-    'selenium',
-    'lxml'
-]
+# https://github.com/avinassh/rockstar/blob/master/setup.py#L11,#L19
+
+version = '2.0'
+
+setup(
+    name='stockspython',
+    version=version,
+    install_requires=[
+        'selenium',
+        'lxml'
+    ],
+    author='Nicholas Langford',
+)
